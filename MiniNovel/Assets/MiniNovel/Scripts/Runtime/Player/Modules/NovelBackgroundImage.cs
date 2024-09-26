fa76dabd9a147248907d71620b3341d52df16cf7
DAIKI <daiki.evilone@gmail.com>
2024-09-26 13:41:35 +0900

    Load background image without scriptable asset.

--- MiniNovel/Assets/MiniNovel/Scripts/Runtime/Player/Modules/NovelBackgroundImage.cs
+++ MiniNovel/Assets/MiniNovel/Scripts/Runtime/Player/Modules/NovelBackgroundImage.cs
@@ -1,6 +1,10 @@
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 namespace MiniNovel.Player
@@ -11,7 +15,7 @@ namespace MiniNovel.Player
         private string _commandName = "back";
 
         [SerializeField]
-        private NovelBackgroundImageSettings _settings = null;
+        private string _folderName = "bgimage";
 
         [SerializeField]
         private Image _image = null;
@@ -21,18 +25,35 @@ namespace MiniNovel.Player
             return textElement.ElementType == TextElementType.Command && textElement.Content == _commandName;
         }
 
-        public override UniTask Execute(TextElement textElement, NovelModulePayload payload, CancellationToken cancellationToken)
+        public override async UniTask Execute(TextElement textElement, NovelModulePayload payload, CancellationToken cancellationToken)
         {
             if (textElement.TryGetStringParameter("name", out var name))
             {
-                if (_settings.TryGetSprite(name, out var sprite))
+                var texture = await FindTexture(name);
+                _image.enabled = texture != null;
+                if (texture != null)
                 {
                     _image.enabled = true;
-                    _image.sprite = sprite;
+                    _image.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
                 }
             }
+        }
 
-            return UniTask.CompletedTask;
+        private async UniTask<Texture2D> FindTexture(string fileName)
+        {
+            var hasExtension = Path.HasExtension(fileName);
+            var streamingAssetsFilePath = Path.Combine(Application.streamingAssetsPath, _folderName);
+            var searchFilter = hasExtension ? new Regex(fileName) : new Regex(fileName + ".*");
+            var file = Directory.GetFiles(streamingAssetsFilePath).Where(fileName => searchFilter.IsMatch(fileName)).FirstOrDefault();
+            var request = UnityWebRequestTexture.GetTexture(file);
+            await request.SendWebRequest();
+            if (request.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError($"{request.result} {request.error}");
+                return null;
+            }
+
+            return DownloadHandlerTexture.GetContent(request);
         }
     }
 }
