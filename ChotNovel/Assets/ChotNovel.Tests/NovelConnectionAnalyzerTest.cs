aaa276c51ef16ac8587f822174763bec16a42958
DAIKI <daiki.evilone@gmail.com>
2024-09-28 08:03:01 +0900

    Added connection analyzer.

---
+++ ChotNovel/Assets/ChotNovel.Tests/NovelConnectionAnalyzerTest.cs
@@ -0,0 +1,135 @@
+using System.Collections.Generic;
+using ChotNovel.Player;
+using NUnit.Framework;
+
+namespace ChotNovel.Tests
+{
+    public class NovelConnectionAnalyzerTest
+    {
+        private NovelConnectionAnalyzer _analyzer;
+        private List<TextElement> _file1Texts;
+        private List<TextElement> _file2Texts;
+        private List<TextElement> _file3Texts;
+
+        [SetUp]
+        public void Setup()
+        {
+            // file1 jump to file1 and file2
+            _file1Texts = new List<TextElement>();
+            {
+                _file1Texts.Add(new TextElement("Start", TextElementType.Label));
+                var jump1 = new TextElement("jump", TextElementType.Command);
+                jump1.AddParameter("label", "Next");
+                _file1Texts.Add(jump1);
+                _file1Texts.Add(new TextElement("Next", TextElementType.Label));
+                var jump2 = new TextElement("jump", TextElementType.Command);
+                jump2.AddParameter("file", "file2");
+                jump2.AddParameter("label", "Start");
+                _file1Texts.Add(jump2);
+
+            }
+            // file2 choice to file2 or file3
+            _file2Texts = new List<TextElement>();
+            {
+                _file2Texts.Add(new TextElement("Start", TextElementType.Label));
+                var choice1 = new TextElement("choice", TextElementType.Command);
+                choice1.AddParameter("label", "Next");
+                _file2Texts.Add(choice1);
+                var choice2 = new TextElement("choice", TextElementType.Command);
+                choice2.AddParameter("file", "file3");
+                choice2.AddParameter("label", "Start");
+                _file2Texts.Add(choice2);
+                _file2Texts.Add(new TextElement("Next", TextElementType.Label));
+            }
+            // file3 no jump
+            _file3Texts = new List<TextElement>();
+            {
+                _file3Texts.Add(new TextElement("Start", TextElementType.Label));
+            }
+            // Push texts
+            _analyzer = new NovelConnectionAnalyzer();
+            _analyzer.PushFileTexts("file1", _file1Texts);
+            _analyzer.PushFileTexts("file2", _file2Texts);
+            _analyzer.PushFileTexts("file3", _file3Texts);
+        }
+
+        [Test]
+        public void GetPreviousEmpty()
+        {
+            var results = _analyzer.GetPreviousLabels("file1", "Start");
+            Assert.AreEqual(0, results.Count);
+        }
+
+        [Test]
+        public void GetNextJumpLocalFile()
+        {
+            var results = _analyzer.GetNextLabels("file1", "Start");
+            Assert.AreEqual(1, results.Count);
+            Assert.AreEqual("file1", results[0].File);
+            Assert.AreEqual("Next", results[0].Label);
+        }
+
+        [Test]
+        public void GetPreviousJumpLocalFile()
+        {
+            var results = _analyzer.GetPreviousLabels("file1", "Next");
+            Assert.AreEqual(1, results.Count);
+            Assert.AreEqual("file1", results[0].File);
+            Assert.AreEqual("Start", results[0].Label);
+        }
+
+        [Test]
+        public void GetNextJumpOtherFile()
+        {
+            var results = _analyzer.GetNextLabels("file1", "Next");
+            Assert.AreEqual(1, results.Count);
+            Assert.AreEqual("file2", results[0].File);
+            Assert.AreEqual("Start", results[0].Label);
+        }
+
+        [Test]
+        public void GetPreviousJumpOtherFile()
+        {
+            var results = _analyzer.GetPreviousLabels("file2", "Start");
+            Assert.AreEqual(1, results.Count);
+            Assert.AreEqual("file1", results[0].File);
+            Assert.AreEqual("Next", results[0].Label);
+        }
+
+        [Test]
+        public void GetNextChoice()
+        {
+            var results = _analyzer.GetNextLabels("file2", "Start");
+            Assert.AreEqual(2, results.Count);
+            Assert.AreEqual("file2", results[0].File);
+            Assert.AreEqual("Next", results[0].Label);
+            Assert.AreEqual("file3", results[1].File);
+            Assert.AreEqual("Start", results[1].Label);
+        }
+
+        [Test]
+        public void GetPreviousChoiceLocalFile()
+        {
+            var results = _analyzer.GetPreviousLabels("file2", "Next");
+            Assert.AreEqual(1, results.Count);
+            Assert.AreEqual("file2", results[0].File);
+            Assert.AreEqual("Start", results[0].Label);
+        }
+
+        [Test]
+        public void GetPreviousChoiceOtherFile()
+        {
+            var results = _analyzer.GetPreviousLabels("file3", "Start");
+            Assert.AreEqual(1, results.Count);
+            Assert.AreEqual("file2", results[0].File);
+            Assert.AreEqual("Start", results[0].Label);
+        }
+
+        [Test]
+        public void GetNextEmpty()
+        {
+            var results = _analyzer.GetNextLabels("file3", "Start");
+            Assert.AreEqual(0, results.Count);
+        }
+    }
+}
---
+++ ChotNovel/Assets/ChotNovel/Scripts/Runtime/Player/Analysis/NovelConnectionAnalyzer.cs
@@ -0,0 +1,111 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace ChotNovel.Player
+{
+    public class NovelConnectionAnalyzer
+    {
+        public class LabelAddress
+        {
+            public readonly string File;
+            public readonly string Label;
+            public LabelAddress(string file, string label)
+            {
+                File = file;
+                Label = label;
+            }
+        }
+
+        private class Connection
+        {
+            public readonly LabelAddress From;
+            public readonly LabelAddress To;
+            public Connection(LabelAddress from, LabelAddress to)
+            {
+                From = from;
+                To = to;
+            }
+        }
+
+        private readonly List<Connection> _connections = new List<Connection>();
+
+        private readonly List<TextElement> _textElementBuffer = new List<TextElement>();
+        public void PushFileTexts(string file, IReadOnlyList<TextElement> textElements)
+        {
+            _textElementBuffer.Clear();
+            foreach (var textElement in textElements)
+            {
+                if (textElement.ElementType == TextElementType.Label && _textElementBuffer.Count > 0)
+                {
+                    PushLabeledSection(file, _textElementBuffer);
+                    _textElementBuffer.Clear();
+                }
+                _textElementBuffer.Add(textElement);
+            }
+            if (_textElementBuffer.Count > 0)
+            {
+                PushLabeledSection(file, _textElementBuffer);
+            }
+        }
+
+        public void PushLabeledSection(string file, IReadOnlyList<TextElement> textElements)
+        {
+            var label = textElements.FirstOrDefault(v => v.ElementType == TextElementType.Label);
+            if (label == null)
+            {
+                Debug.LogError("Label not found in labeled section.");
+                return;
+            }
+
+            var jumpElements = textElements
+                .Where(v => v.ElementType == TextElementType.Command)
+                .Where(v => v.Content == "jump" || v.Content == "choice");
+            foreach (var jumpElement in jumpElements)
+            {
+                var from = new LabelAddress(file, label.Content);
+                var to = new LabelAddress(
+                    jumpElement.TryGetStringParameter("file", out var fileValue) ? fileValue : file,
+                    jumpElement.TryGetStringParameter("label", out var labelValue) ? labelValue : string.Empty);
+                if (string.IsNullOrEmpty(to.Label))
+                {
+                    Debug.LogError($"label not found in command. file:{file}, command:{jumpElement.Content}");
+                    continue;
+                }
+                _connections.Add(new Connection(from, to));
+            }
+        }
+
+        public List<LabelAddress> GetPreviousLabels(string file, string label)
+        {
+            var results = new List<LabelAddress>();
+            GetPreviousLabels(file, label, results);
+            return results;
+        }
+
+        public void GetPreviousLabels(string file, string label, List<LabelAddress> results)
+        {
+            results.Clear();
+            var connectedLabels = _connections
+                .Where(v => v.To.File == file && v.To.Label == label)
+                .Select(v => v.From);
+            results.AddRange(connectedLabels);
+        }
+
+        public List<LabelAddress> GetNextLabels(string file, string label)
+        {
+            var results = new List<LabelAddress>();
+            GetNextLabels(file, label, results);
+            return results;
+        }
+
+        public void GetNextLabels(string file, string label, List<LabelAddress> results)
+        {
+            results.Clear();
+            var connectedLabels = _connections
+                .Where(v => v.From.File == file && v.From.Label == label)
+                .Select(v => v.To);
+            results.AddRange(connectedLabels);
+        }
+    }
+}
